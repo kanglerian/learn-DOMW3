d3b8e77bf6285f1503e5bf8704c258488d75c557
kanglerian <kanglerian@gmail.com>
2021-01-03 13:50:50 +0700

    Document Apps

---
+++ APPSDOM/js/documentObject.js
@@ -0,0 +1,36 @@
+var pesan = "Semangat terus belajarnya! kamu pasti bisa :) ";
+document.getElementById("jalan ").innerHTML = pesan;
+
+function myFunction() {
+    document.getElementById("demo ").innerHTML =
+        "Terakhir di modifikasi jam segini " +
+        "<b>" + document.lastModified + "</b>";
+}
+
+
+function bukaBaru() {
+    var w = window.open();
+    w.document.open();
+    w.document.write("<h2>Hello World!</h2>");
+    w.document.close();
+}
+
+function lihatTitle() {
+    alert(document.title);
+}
+
+function lihatURL() {
+    alert(document.URL);
+}
+
+var btn = document.getElementsByClassName("btn");
+
+function lihatButton() {
+    alert(btn.length);
+}
+
+var img = document.getElementsByTagName("img");
+
+function lihatGambar() {
+    alert(img.length);
+}
---
+++ APPSDOM/js/script.js
@@ -0,0 +1,6 @@
+var date = new Date();
+var websiteku = "CodaleCenter";
+var kota = "Tasikmalaya";
+// @ts-ignore
+document.getElementById("tahun").innerHTML =
+    "© " + 2020 + " - " + date.getFullYear() + " " + websiteku + " • All rights reserved • love from " + kota;
