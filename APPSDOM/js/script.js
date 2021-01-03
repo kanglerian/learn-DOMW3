c83ad14294f534b3c19098b2758a042d262d1e7f
kanglerian <kanglerian@gmail.com>
2021-01-03 15:16:17 +0700

    dark mode

--- APPSDOM/js/script.js
+++ APPSDOM/js/script.js
@@ -8,3 +8,21 @@ document.getElementById("tahun").innerHTML =
 
 var pesan = "Semangat terus belajarnya! kamu pasti bisa :) ";
 document.getElementById("jalan").innerHTML = pesan;
+
+document.getElementById("title").innerHTML = document.title;
+
+function darkMode() {
+    document.body.style.backgroundColor = "#111111";
+    document.body.style.color = "white";
+    document.getElementById("p0").style.backgroundColor = "#1E1E1E";
+    document.getElementById("p2").style.backgroundColor = "#1E1E1E";
+    document.getElementById("p3").style.backgroundColor = "#1E1E1E";
+}
+
+function lightMode() {
+    document.body.style.backgroundColor = "#fafafa";
+    document.body.style.color = "black";
+    document.getElementById("p0").style.backgroundColor = "#fafafa";
+    document.getElementById("p2").style.backgroundColor = "white";
+    document.getElementById("p3").style.backgroundColor = "white";
+}
