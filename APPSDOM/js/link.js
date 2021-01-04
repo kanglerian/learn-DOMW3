c7e5613747fb97016639efb7d3128e3147a53995
kanglerian <kanglerian@gmail.com>
2021-01-04 18:10:12 +0700

    up

--- APPSDOM/js/link.js
+++ APPSDOM/js/link.js
@@ -10,3 +10,22 @@ function dua() {
     document.getElementById("demo").innerHTML =
         "Linknya ini ya : " + "<b>" + document.links[1].href + "</b>";
 }
+
+function berubah() {
+
+    var br = document.getElementById("in1").value = "Nah kan ini berubah?";
+}
+
+function tampilkan() {
+    var tpl = document.getElementById("frm1");
+    var txt = "";
+    var i;
+    for (i = 0; i < tpl.length; i++) {
+        txt += tpl.elements[i].value + "<br>";
+    }
+    document.getElementById("demo2").innerHTML = txt;
+}
+
+function reset() {
+    document.getElementById("frm1").reset();
+}
